44a6ed610d167d6f9aed99eed242d4ab10cc82b0
Josephtran102 <josephtran0505@gmail.com>
2024-01-01 10:53:07 +0700

    Commit message

---
+++ utils/constants.ts
@@ -0,0 +1,6 @@
+import { BaseGoerli } from "@thirdweb-dev/chains";
+
+export const CLIENT_ID = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "";
+export const CHAIN = BaseGoerli;
+export const CONTRACT_ADDR = "0x5370B740616502979Fb1122E5e3896D41f6DAA1B";
+export const FACTORY_ADDR = "0x7f2A5F9C7B90C6B4F46038445118a9EB288722dd";
