5a63658a72e007e3cc7da574ad586a4369c978b6
Josephtran102 <josephtran0505@gmail.com>
2024-01-01 14:40:23 +0700

    Commit message

--- components/header.tsx
+++ components/header.tsx
@@ -1,34 +1,33 @@
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import Image from "next/image";
-import { Aurora } from "./Aurora";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   const address = useAddress();
+  
   return (
-    <header className="w-full p-4 mb-12">
-      <Aurora
-        size={{ width: "1800px", height: "700px" }}
-        pos={{ top: "0%", left: "50%" }}
-        color="hsl(277deg 59% 39% / 20%)"
-      />
-      <div className="max-w-5xl mx-auto flex justify-between items-center ">
+    <header className="w-full p-4 mb-12 relative">
+      <div className="max-w-5xl mx-auto flex justify-between items-center relative z-10">
         <div>
-          <Image
-            className="md:hidden"
-            width={48}
-            height={48}
-            src="/logo.png"
-            alt="Joseph"
-          />
-          <Image
-            className="hidden md:block"
-            width={194}
-            height={28}
-            src="/logo.png"
-            alt="thirdweb"
-          />
+          <Link href="https://catattack.vercel.app/" passHref>
+            <div className="your-button-styles">
+              <Image
+                className="md:hidden"
+                width={48}
+                height={48}
+                src="/logo.png"
+                alt="Joseph"
+              />
+              <Image
+                className="hidden md:block"
+                width={194}
+                height={28}
+                src="/logo.png"
+                alt="thirdweb"
+              />
+            </div>
+          </Link>
         </div>
-
         <div className="max-w-xs">{address ? <ConnectWallet /> : null}</div>
       </div>
     </header>
--- utils/constants.ts
+++ utils/constants.ts
@@ -2,5 +2,5 @@ import { BaseGoerli } from "@thirdweb-dev/chains";
 
 export const CLIENT_ID = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "";
 export const CHAIN = BaseGoerli;
-export const CONTRACT_ADDR = "0xA564A59d17631F1C546CD87313D903BB99b01E8b";
+export const CONTRACT_ADDR = "0x28BF2ad6AeDA686709e8244c656a4D9c137271a5";
 export const FACTORY_ADDR = "0x7f2A5F9C7B90C6B4F46038445118a9EB288722dd";
