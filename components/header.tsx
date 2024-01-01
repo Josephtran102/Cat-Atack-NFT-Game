1100dd907e254678e42ec902ee1415a92ad6337a
Josephtran102 <josephtran0505@gmail.com>
2024-01-01 14:49:27 +0700

    Commit message

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,18 @@
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import Image from "next/image";
 import Link from "next/link";
+import { Aurora } from "./Aurora"; // Bổ sung import cho Aurora
 
 const Header: React.FC = () => {
   const address = useAddress();
   
   return (
     <header className="w-full p-4 mb-12 relative">
+      <Aurora  // Bổ sung lại đoạn code Aurora ở đây
+        size={{ width: "1800px", height: "700px" }}
+        pos={{ top: "0%", left: "50%" }}
+        color="hsl(277deg 59% 39% / 20%)"
+      />
       <div className="max-w-5xl mx-auto flex justify-between items-center relative z-10">
         <div>
           <Link href="https://catattack.vercel.app/" passHref>
